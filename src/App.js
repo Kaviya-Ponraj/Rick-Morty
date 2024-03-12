9daa58f422b7aac075a9a9089d6e761393001311
Kaviya-Ponraj <124489487+Kaviya-Ponraj@users.noreply.github.com>
2024-03-12 20:09:17 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+// import "./App.css"
+
+import Filters from "./Components/Filters/Filters";
+import Cards from "./Components/Cards/Cards"
+import Pagination from './Components/Pagination/Pagination';
+import Search from './Components/Search/Search';
+import Navbar from './Components/Navbar/Navbar';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Episodes from './Pages/Episodes';
+import Location from './Pages/Location';
+import CardDetails from './Components/Cards/CardDetails';
+
+
+function App(){
+  return (
+    <Router>
+      <div className="App">
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/:id' element={<CardDetails />}/>
+
+        
+        <Route path='/episodes' element={<Episodes />}/>
+        <Route path='/episodes/:id' element={<CardDetails />}/>
+
+        <Route path='/location' element={<Location />}/>
+        <Route path='/location/:id' element={<CardDetails />}/>
+      </Routes>
+      </div>
+    </Router>
+  )
+}
+
+
+const Home = () => {
+  let [ pageNumber, setpageNumber] = useState(1);
+  // console.log(pageNumber);
+
+  let [search, setSearch] = useState("")
+
+  let [fetchData, updateFetchData] = useState([]);
+
+  let [status, setStatus] = useState("")
+  let [gender, setGender] = useState("")
+  let [species, setSpecies] = useState("")
+
+  let {info, results} = fetchData
+  // console.log(fetchData)
+  // console.log(info);
+  // console.log(results);
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`
+
+  useEffect(()=>{
+    ( async function() {
+      let data = await fetch(api).then((res) => res.json())
+      // console.log(data.results)
+      updateFetchData(data)
+    }())
+  }, [api])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <h1 className='text-center mb-4'>Characters</h1>
+
+    <Search 
+    setpageNumber = {setpageNumber}
+    setSearch = {setSearch}
+    />
+
+    <div className="container">
+      <div className="row">
+        
+          <Filters
+          setpageNumber = {setpageNumber}
+          setStatus = {setStatus}
+          setGender = {setGender}
+          setSpecies = {setSpecies}
+          />
+       
+        <div className="col-lg-8 col-12">
+          <div className="row">
+            <Cards
+            page = "/"
+            results={results} />
+            {/* <Cards />
+            <Cards /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <Pagination 
+     info = {info}
+    pageNumber = {pageNumber}
+    setpageNumber = {setpageNumber}
+    />
     </div>
   );
 }
